402ddf3f53ad8c04fa77414f3c8a641f15913dfb
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-10-17 01:11:11 +0900

    [Fix] #66 clapsテーブルのuser_idの非NULL制約を外す
    ユーザーが削除されてもclapsテーブルのカウントは残したいため、
    ユーザー削除時にnullが設定されるようにしました。

--- db/migrate/20231002081827_create_claps.rb
+++ db/migrate/20231002081827_create_claps.rb
@@ -1,7 +1,7 @@
 class CreateClaps < ActiveRecord::Migration[7.0]
   def change
     create_table :claps do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :diary, null: false, foreign_key: true
       t.integer :count, null: false, default: 0
 
