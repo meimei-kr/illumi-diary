dd537cff743e222629c99f2f94e0182899720e06
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-10-19 18:10:03 +0900

    [Fix] アップローダーに環境ごとの保存場所を設定

--- app/uploaders/character_image_uploader.rb
+++ app/uploaders/character_image_uploader.rb
@@ -6,8 +6,11 @@ class CharacterImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog              # 本番時にS3にファイルを保存する
+  else
+    storage :file             # 開発・テスト時にはローカルにファイルを保存する
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
