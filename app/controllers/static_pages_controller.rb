214eb3cba8a89a4a415a5159125be63a6114df91
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-11-13 22:11:56 +0900

    [Add] プライバシーポリシーの追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top terms]
+  skip_before_action :require_login, only: %i[top terms privacy_policy]
 
   def top; end
 
   def terms; end
+
+  def privacy_policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#top'
   get 'terms', to: 'static_pages#terms'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
