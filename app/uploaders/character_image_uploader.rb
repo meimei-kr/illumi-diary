b6884ef4028864471fad18194a90c4f979020dbc
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-11-16 23:06:44 +0900

    [Update] #69 容量の大きい画像をWebpに変更

--- app/uploaders/character_image_uploader.rb
+++ app/uploaders/character_image_uploader.rb
@@ -20,7 +20,7 @@ class CharacterImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    ActionController::Base.helpers.asset_path('default_character.png')
+    ActionController::Base.helpers.asset_path('default_character.webp')
   end
 
   # Process files as they are uploaded:
